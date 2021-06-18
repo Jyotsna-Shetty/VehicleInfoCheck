a7754eedfd0cd3582980b1d78308b4fac5fea3bd
Jyotsna-Shetty <jyotssystems@gmail.com>
2021-06-18 13:16:11 +0530

    Update Image Activity

--- app/src/main/java/com/example/vehicleinfocheck/ImageActivity.java
+++ app/src/main/java/com/example/vehicleinfocheck/ImageActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -36,6 +37,7 @@ public class ImageActivity extends AppCompatActivity {
     public static final int GALLERY_REQUEST_CODE = 105;//gallery request code
     ImageView selectedImage;//import Imageview variable as selectedImage
     Button cameraBtn,galleryBtn, ScanBtn;//import camera and gallery button
+    TextView sampleImgText;
     String currentPhotoPath;
 
     @Override
@@ -45,6 +47,7 @@ public class ImageActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT); //fixes orientation to PORTRAIT mode
 
         selectedImage = findViewById(R.id.displayImageView); //selecting imageview using id from xml resources
+        sampleImgText = findViewById(R.id.SampleImgMsg);
         cameraBtn = findViewById(R.id.cameraBtn); //selecting camera using id from xml resources
         galleryBtn = findViewById(R.id.galleryBtn); //selecting gallery using id from xml resources
         ScanBtn = findViewById(R.id.ScanBtn); //selecting scan button using id from xml resources
@@ -112,8 +115,8 @@ public class ImageActivity extends AppCompatActivity {
             //checking resultCode
             if (resultCode == Activity.RESULT_OK) {
                 File f = new File(currentPhotoPath);//creating new file f from the currentPhotoPath
-                selectedImage.setVisibility(View.VISIBLE);
                 selectedImage.setImageURI(Uri.fromFile(f));//set image to imageview using uri
+                sampleImgText.setVisibility(View.INVISIBLE);
                 Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f));//display absolute url of the file
 
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
@@ -129,8 +132,8 @@ public class ImageActivity extends AppCompatActivity {
                 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());//creating file name using time stamp
                 String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
                 Log.d("tag", "onActivityResult: Gallery Image Uri: " + imageFileName);
-                selectedImage.setVisibility(View.VISIBLE);
                 selectedImage.setImageURI(contentUri);
+                sampleImgText.setVisibility(View.INVISIBLE);
             }
         }
     }
