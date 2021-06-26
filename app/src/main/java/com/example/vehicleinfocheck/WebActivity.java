a772e5b2b8c54a8b7c198da7dd2eda29a81d95f9
Jyotsna-Shetty <81788169+Jyotsna-Shetty@users.noreply.github.com>
2021-06-26 19:38:04 +0530

    Update WebActivity.java

--- app/src/main/java/com/example/vehicleinfocheck/WebActivity.java
+++ app/src/main/java/com/example/vehicleinfocheck/WebActivity.java
@@ -11,34 +11,36 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.ClipData;
 
-// Class that displays VAHAN website in the app
+//Class that displays VAHAN website in the app
 public class WebActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);   // Fixes orientation to PORTRAIT mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);   //Fixes orientation to PORTRAIT mode
         EditText vehicleNumber = findViewById(R.id.VehicleNum);
         ImageButton CopyButton = findViewById(R.id.CopyBtn);
         LinearLayout webview= findViewById(R.id.Webview);
-        WebScraper webScraper = new WebScraper(this);   // Creating an instance of the WebScraper class
+        WebScraper webScraper = new WebScraper(this);   //Creating an instance of the WebScraper class
 
-        webScraper.setUserAgentToDesktop(true);                 // Default: false
-        webScraper.setLoadImages(true);                         // Default: false
-        webview.addView(webScraper.getView());                  // Setting the LinearLayout to display the contents of the webScraper
+        webScraper.setUserAgentToDesktop(true);                 //Default: false
+        webScraper.setLoadImages(true);                         //Default: false
+        webview.addView(webScraper.getView());                  //Setting the LinearLayout to display the contents of the webScraper
         String FullUrl = "https://vahan.nic.in/nrservices/faces/user/searchstatus.xhtml";
         webScraper.loadURL(FullUrl);
         Toast.makeText(this, "Loading website...", Toast.LENGTH_SHORT).show();
 
-        // OnClickListener triggered when CopyButton is clicked
+        //OnClickListener triggered when CopyButton is clicked
         CopyButton.setOnClickListener(v -> {
-            // Creating an instance of ClipboardManager class
+            //Creating an instance of ClipboardManager class
             ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-            if (!vehicleNumber.getText().toString().trim().equals("")) {                        // Clip is made only if there is text to copy
-                ClipData clip = ClipData.newPlainText("simple text", vehicleNumber.getText());  // Creates a clip with value = vehicleNumber
+            if (!vehicleNumber.getText().toString().trim().equals("")) {                        //Clip is made only if there is text to copy
+                ClipData clip = ClipData.newPlainText("simple text", vehicleNumber.getText());  //Creates a clip with value = vehicleNumber
                 clipboard.setPrimaryClip(clip);
                 Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show();         
+            } else {
+                Toast.makeText(this,"No text to copy",Toast.LENGTH_SHORT).show();
             }
         });
     }
