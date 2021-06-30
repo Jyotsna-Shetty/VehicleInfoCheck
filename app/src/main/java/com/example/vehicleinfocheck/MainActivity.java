e6bb1fb10829d97d4ce8e91161f94b668dd2b220
Jyotsna-Shetty <81788169+Jyotsna-Shetty@users.noreply.github.com>
2021-06-30 22:02:09 +0530

    Comments

--- app/src/main/java/com/example/vehicleinfocheck/MainActivity.java
+++ app/src/main/java/com/example/vehicleinfocheck/MainActivity.java
@@ -9,22 +9,20 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-// Class that contains layout of the complete app
+//Class that contains layout of the complete app
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);//to appear in full screen
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //to appear in full screen
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
         //splash screen appears for 2.5 seconds
         int SPLASH_SCREEN_TIME_OUT = 2500;
         new Handler().postDelayed(() -> {
-            Intent image=new Intent(MainActivity.this,
-                    ImageActivity.class);
+            Intent image=new Intent(MainActivity.this, ImageActivity.class);
             //Intent is used to switch from one activity to another.
 
             startActivity(image);
